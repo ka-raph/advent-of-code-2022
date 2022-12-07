403883cdc513d5e6fbc7b954fd2715d87b00103c
Raphael Karampournis <raphael.karampournis@lexisnexisrisk.com>
2022-12-07 10:28:46 +0100

    updated day 6 with better solution

--- day6/index.js
+++ day6/index.js
@@ -1,34 +1,17 @@
 import puzzleInput from "./puzzleInput.js";
 
-// Can break if `i` is greater than `pouzzleInput.length - markerLength`, it can be fixed by adding an extra check in the if statement
+// Can break if `i` is greater than `puzzleInput.length - markerLength`, it can be fixed by adding an extra check in the if statement. Not needed for that challenge.
 
 let markerLength = 4;   // Size of the bulk of characters that need to be checked
-for (let i = 0; i < puzzleInput.length; i++) {
-    const arr = [puzzleInput[i]];
-    for (let j = 1; j < markerLength; j++) {
-        if (!arr.includes(puzzleInput[i+j])) arr.push(puzzleInput[i+j]);
-        else {
-            break;
+console.log('Part 1:', getIndexAfterMarker(markerLength)); // 1647
+
+markerLength = 14;
+console.log('Part 2:', getIndexAfterMarker(markerLength)); // 2447
+
+function getIndexAfterMarker(length) {
+    for (let i = 0; i < puzzleInput.length; i++) {
+        if (new Set(puzzleInput.slice(i, i + length)).size === length) { // Duplicates are not added twice to the Set, therefore it's length is lower if there are any
+            return i + length;
         }
-    }
-    if (arr.length === markerLength) {
-        console.log(i + markerLength); // 1647
-        break;
     }
 }
-
-// PART 2
-markerLength = 14;
-for (let i = 0; i < puzzleInput.length; i++) {
-    const arr = [puzzleInput[i]];
-    for (let j = 1; j < 14; j++) {
-        if (!arr.includes(puzzleInput[i+j])) arr.push(puzzleInput[i+j]);
-        else {
-            break;
-        }
-    }
-    if (arr.length === markerLength) {
-        console.log(i + markerLength); // 2447
-        break;
-    }
-} 
